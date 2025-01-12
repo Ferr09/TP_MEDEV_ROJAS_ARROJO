fbd40e6755c63064897c069e5f45a26c49e262e1
Fernando Rojas Rivera <90526866+Ferr09@users.noreply.github.com>
2025-01-12 17:36:33 +0100

    Add files via upload

---
+++ tp/src/main/java/fr/centrale/tp_medev/Tp_medev.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package fr.centrale.tp_medev;
+
+/**
+ *
+ * @author fe_a8
+ */
+public class Tp_medev {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
