16c7c31d85455aa711e8470effc92431311aade3
Augusto Arrojo <augusto.arrojo@eleves.ec-nantes.fr>
2025-01-13 11:33:04 +0100

    InitialBoardSetup

---
+++ tp/src/test/java/fr/centrale/tp_medev/BoardTest.java
@@ -0,0 +1,48 @@
+package fr.centrale.tp_medev;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Test class for the Board class. This class contains unit tests to validate
+ * the functionality of the Board class.
+ */
+public class BoardTest {
+
+    private Board board;
+
+    /**
+     * Set up the board before each test. This method initializes an 8x8 board
+     * with the default starting configuration.
+     */
+    @BeforeEach
+    public void setUp() {
+        board = new Board(8); // Initialize the board
+        board.initializeBoard(); // Ensure the board is set up correctly
+    }
+
+    /**
+     * Tests the initial setup of the board. Ensures that the pieces are placed
+     * correctly at the start of the game.
+     */
+    @Test
+    public void testInitialBoardSetup() {
+        // Check the initial setup (the center of the board should have the starting pieces)
+        assertEquals(Board.WHITE, board.getBoard()[3][3], "Center (3, 3) should be WHITE");
+        assertEquals(Board.BLACK, board.getBoard()[3][4], "Center (3, 4) should be BLACK");
+        assertEquals(Board.BLACK, board.getBoard()[4][3], "Center (4, 3) should be BLACK");
+        assertEquals(Board.WHITE, board.getBoard()[4][4], "Center (4, 4) should be WHITE");
+
+        // All other cells should be empty
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                // Exclude the center positions
+                if ((i != 3 || j != 3) && (i != 3 || j != 4) && (i != 4 || j != 3) && (i != 4 || j != 4)) {
+                    assertEquals(Board.EMPTY, board.getBoard()[i][j], "Position (" + i + ", " + j + ") should be EMPTY");
+                }
+            }
+        }
+    }
+}
