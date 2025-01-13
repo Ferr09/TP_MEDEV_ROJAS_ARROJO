bd01e4cfad587c54b154bbdb7f013cbf2612fc81
Fernando Rojas Rivera <90526866+Ferr09@users.noreply.github.com>
2025-01-13 11:23:13 +0100

    Creation of OthelloGame test file

---
+++ tp/src/test/java/fr/centrale/tp_medev/OthelloGameTest.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fr.centrale.tp_medev;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+/**
+ *
+ * @author fe_a8
+ */
+public class OthelloGameTest {
+    
+}
