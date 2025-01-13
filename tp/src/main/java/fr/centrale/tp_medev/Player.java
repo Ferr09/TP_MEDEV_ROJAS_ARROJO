fb751fd41486bb7f3ea485c1224a9708a62dd33a
Augusto Arrojo <augusto.arrojo@eleves.ec-nantes.fr>
2025-01-13 09:49:17 +0100

    Player's name

--- tp/src/main/java/fr/centrale/tp_medev/Player.java
+++ tp/src/main/java/fr/centrale/tp_medev/Player.java
@@ -1,9 +1,30 @@
 package fr.centrale.tp_medev;
 
 /**
+ * This class represents a player in the game.
  *
  * @author Augusto ARROJO et Fernando Rojas
  */
 public class Player {
-    
+
+    private final String name;
+
+    /**
+     * Constructs a new player.
+     *
+     * @param name The name of the player.
+     * @param symbol The symbol representing the player ('B' or 'W').
+     */
+    public Player(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Gets the player's name.
+     *
+     * @return The name of the player.
+     */
+    public String getName() {
+        return name;
+    }
 }
