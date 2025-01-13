08f21be6b843abcfb9937d252239e50073d13fc5
Augusto Arrojo <augusto.arrojo@eleves.ec-nantes.fr>
2025-01-13 09:01:48 +0100

    Creation de la classe OthelloGame

---
+++ tp/src/main/java/fr/centrale/tp_medev/OthelloGame.java
@@ -0,0 +1,12 @@
+package fr.centrale.tp_medev;
+
+/**
+ *
+ * @author Augusto ARROJO et Fernando Rojas
+ */
+public class OthelloGame {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
